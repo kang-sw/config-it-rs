1395104c5b4773e5574f0679c688cd53a7f4675b
ki6080 <ki6080@gmail.com>
2022-10-26 23:39:13 +0900

    [wip] Learning Rust

--- core_features/src/config.rs
+++ core_features/src/config.rs
@@ -1,12 +1,67 @@
-use serde::{Serialize};
+use std::ops::Deref;
+use std::string::String;
+use std::{any::Any, marker::PhantomData, rc::Rc};
 
-#[derive(Serialize)]
-pub struct ConfigMetadata {
-    name: String,
-    description: String,
-    
+pub mod back {
+    use super::*;
+
+    pub struct ConfigMetadata {
+        pub name: &'static str,
+        pub description: &'static str,
+    }
+
+    pub struct ConfigEntityBase {
+        pub meta: &'static ConfigMetadata,
+        pub body: Rc<dyn Any>,
+    }
 }
 
-pub struct ConfigEntity {}
+pub struct ConfigEntity<T: Copy> {
+    _p0: PhantomData<T>,
+    base: back::ConfigEntityBase,
+    fence: u64,
+}
 
-pub mod back {}
+impl<T: 'static + Copy> ConfigEntity<T> {
+    pub fn refer(&self) -> &T {
+        (self.base.body.deref())
+            .downcast_ref::<T>()
+            .unwrap()
+    }
+
+    pub fn value(&self) -> T {
+        self.refer().clone()
+    }
+
+    pub fn check_update(&mut self) -> bool {
+        false
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn try_compile() {
+        static e: back::ConfigMetadata = back::ConfigMetadata {
+            name: "MyNameIs",
+            description: "AlphaBetaGamma",
+        };
+
+        let s = ConfigEntity::<bool> {
+            _p0: PhantomData,
+            base: back::ConfigEntityBase {
+                meta: &e,
+                body: Rc::new(false),
+            },
+            fence: 1,
+        };
+
+        {
+            let fg = s.refer();
+            assert!(!fg);
+        }
+
+        let other = s;
+    }
+}
--- core_features/src/terminal.rs
+++ core_features/src/terminal.rs
@@ -1,4 +1,1 @@
-struct TerminalContext {
-    
-}
 
