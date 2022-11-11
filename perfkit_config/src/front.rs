18074b8e3afb9ceac643ce60787f97edef322b21
kang-sw <ki6080@gmail.com>
2022-11-11 21:48:31 +0900

    front: Add example  machine-gen code

--- perfkit_config/src/front.rs
+++ perfkit_config/src/front.rs
@@ -67,10 +67,9 @@ use std::sync::Arc;
 ///
 ///
 ///
-pub trait ReflectCollection {
-    fn __entity_get_meta(&self, at: usize) -> Arc<Metadata> {
-        todo!()
-    }
+pub trait ConfigSetReflection {
+    fn __get_offset_idx_table() -> Arc<HashMap<usize, usize>>;
+    fn __entity_get_meta(&self) -> Arc<Vec<Metadata>>;
 }
 
 ///
@@ -86,8 +85,8 @@ pub struct ConfigSet<T> {
     /// than this can be updated. After update, this number will be set as largest value
     /// of all updated entities.
     update_fence: usize,
-    
-    /// 
+
+    ///
     body: T,
 }
 
@@ -102,7 +101,7 @@ struct CollectionDescriptor {
     config_base_set: Vec<Arc<EntityBase>>,
 }
 
-impl<T: ReflectCollection> ConfigSet<T> {
+impl<T: ConfigSetReflection> ConfigSet<T> {
     // TODO: Create collection with storage
     fn create(s: Storage, prefix: impl Iterator<Item = &'static str>) {
         // 1. Create default T, retrieve offset-index mapping
@@ -112,7 +111,13 @@ impl<T: ReflectCollection> ConfigSet<T> {
     fn update(&self) -> bool {
         todo!()
 
-        // 1. Submit update_fence value to storage, then storage returns
+        // 0. Storage update is performed in background. (By replacing ConfigEntityBase's
+        //     value pointer & update fence). From this operation, config storage's update fence
+        //     value will be updated either.
+
+        // 1. Compare update fence value in storage level.
+
+        // 2. Iterate each config entities, collect all updates, and apply changes to local cache.
     }
 
     /// Check if given element
@@ -131,10 +136,62 @@ impl<T> Deref for ConfigSet<T> {
 
 #[cfg(test)]
 mod test_concepts {
+    use once_cell::sync::{Lazy, OnceCell};
+
     use super::*;
 
+    struct MyTestConfigSet {
+        my_ivar: i32,
+        my_fvar: f32,
+        my_dvar: f64,
+    }
+
+    impl ConfigSetReflection for MyTestConfigSet {
+        fn __get_offset_idx_table() -> Arc<HashMap<usize, usize>> {
+            static LAZY_TABLE: OnceCell<Arc<HashMap<usize, usize>>> = OnceCell::new();
+            return LAZY_TABLE
+                .get_or_init(|| {
+                    let mut table = <HashMap<usize, usize>>::with_capacity(3);
+                    unsafe {
+                        let v = 0 as *const MyTestConfigSet;
+                        table.insert(&(*v).my_ivar as *const i32 as usize, 0);
+                        table.insert(&(*v).my_fvar as *const f32 as usize, 1);
+                        table.insert(&(*v).my_dvar as *const f64 as usize, 2);
+                    }
+
+                    Arc::new(table)
+                })
+                .clone();
+        }
+
+        fn __entity_get_meta(&self) -> Arc<Vec<Metadata>> {
+            static LAZY_TABLE: OnceCell<Arc<Vec<Metadata>>> = OnceCell::new();
+            return LAZY_TABLE
+                .get_or_init(|| {
+                    let mut table = Vec::<Metadata>::with_capacity(3);
+                    table.push(Metadata::create_base("my_ivar".into(), 0));
+                    table[0].description = "hello".into();
+                    table[0].hidden = false;
+
+                    table.push(Metadata::create_base("my_fvar".into(), 0));
+
+                    table.push(Metadata::create_base("my_dvar".into(), 0));
+                    Arc::new(table)
+                })
+                .clone();
+        }
+    }
+
     #[test]
-    fn test_if_compiled() {}
+    fn test_if_compiled() {
+        unsafe {
+            let v = 0 as *const MyTestConfigSet;
+            let addr_1 = &(*v).my_ivar as *const i32 as *const u8;
+            let addr_1 = addr_1 as usize;
+
+            println!("addr_1: {addr_1}")
+        }
+    }
 
     #[test]
     fn test_pseudo_automation() {}
