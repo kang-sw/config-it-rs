08e7e231cadfbddb8dd4e7a997205a8894990848
kang-sw <ki6080@gmail.com>
2023-06-18 12:12:03 +0900

    access.client: Configure small-binary for release

--- config-it-access/client/src/main.rs
+++ config-it-access/client/src/main.rs
@@ -7,7 +7,34 @@ fn main() {
     dioxus_web::launch(App);
 }
 
+pub(crate) mod base {
+    use lazy_static::lazy_static;
+
+    pub fn get_base_url() -> &'static str {
+        lazy_static! {
+            static ref BASE_URL: String = web_sys::window().unwrap().location().origin().unwrap();
+        }
+
+        &BASE_URL
+    }
+
+    #[macro_export]
+    macro_rules! base_url {
+        ($($args:tt)*) => {
+            format!("{}{}", crate::base::get_base_url(), format_args!($($args)*))
+        };
+    }
+}
+
 fn App(cx: Scope) -> Element {
+    use_future!(cx, || async move {
+        reqwest::Client::new()
+            .get(base_url!("/api/test"))
+            .send()
+            .await
+            .unwrap();
+    });
+
     cx.render(rsx! {
         div { class: "w-full h-screen bg-gray-300 flex items-center justify-center", "Hello, world!" }
     })
